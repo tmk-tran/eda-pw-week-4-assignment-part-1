fb1c15056ad7bbfc0212f499967f9077d55776ca
T Mark <tmk1.tran@gmail.com>
2023-08-07 13:02:29 -0500

    completed Week 4, Part 1

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -53,7 +53,7 @@ console.log( times3 );
 //    or greater than zero, and false otherwise
 
 function isPositive(number) {
-  if (number > 0) {
+  if ( number > 0 ) {
     return true;
   } else {
     return false;
@@ -108,7 +108,7 @@ console.log( find( numbers, 4) );
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 
-function isFirstLetter(letter, string) {
+function isFirstLetter( letter, string ) {
     return string.charAt(0) === letter;
 }
 
@@ -116,7 +116,7 @@ console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
 console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll(array) {
+function sumAll( array ) {
   let sum = 0;
   // TODO: loop to add items
   for ( num of array ) {
@@ -135,9 +135,9 @@ console.log( sumAll(array1) );
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
-function allPositive(input) {
+function allPositive( input ) {
   const positiveNums = [];
-  for ( number of (input) ) {
+  for ( number of ( input ) ) {
     if ( number > 0 ) {
       positiveNums.push( number );
     }
