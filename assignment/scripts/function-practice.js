6d5d84e85f6b92bb2e6a82e68ac17830a90e6cf9
T Mark <tmk1.tran@gmail.com>
2023-08-07 11:45:13 -0500

    save

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -80,6 +80,8 @@ function getLast(array) {
   }
 }
 
+const emptyArray = [];
+console.log( getLast(emptyArray) );
 
 
 // 7. Function to find a value in an array. Return true if the 
@@ -95,24 +97,39 @@ function find(value, array) {
   return false;
 }
 
+const numbers = [ 1, 2, 3, 4, 5 ];
+console.log( find( numbers, 4) );
+
+
+
 // ----------------------
 // Stretch Goals
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string) {
 
+function isFirstLetter(letter, string) {
+    return string.charAt(0) === letter;
 }
+
 console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
 console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll() {
+function sumAll(array) {
   let sum = 0;
   // TODO: loop to add items
-
+  for ( num of array ) {
+    sum += num;
+  }
   // TODO: return the sum
+  return sum;
 }
+
+const array1 = [ 5, 6, 8, 9, 11 ];
+console.log( sumAll(array1) );
+
+
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
